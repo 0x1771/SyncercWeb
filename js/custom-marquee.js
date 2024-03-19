8079fb1931e78713393c5a0a5907549a4f0c9486
Mikail Üstüner <163590351+0x1771@users.noreply.github.com>
2024-03-19 14:34:02 +0300

    deleted old resources

--- js/custom-marquee.js
+++
@@ -1,1 +0,0 @@
-$(function(){$(".de-marquee-list").marquee({direction:"left",duration:5e4,gap:0,delayBeforeStart:0,duplicated:!0,startVisible:!0}),$(".wm-carousel").marquee({direction:"left",duration:2e4,gap:100,delayBeforeStart:0,duplicated:!0,startVisible:!1})});
